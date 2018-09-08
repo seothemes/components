5f5b4e014d081bff8a113577f36c67ae0c1fff7b
seothemes <seothemeswp@gmail.com>
2018-09-08 16:44:05 +0800

    Fix page layouts bug

--- src/PageLayouts.php
+++ src/PageLayouts.php
@@ -20,17 +20,17 @@ namespace SeoThemes\Core;
  *
  * $d2_layouts = [
  *     GenesisLayout::REGISTER   => [
- *         'slim-content', [
- *             'label' => 'Slim Content Area',
- *             'image' => get_stylesheet_directory_uri() . '/images/slim-content-icon.png',
+ *         'custom-layout', [
+ *             'label' => 'Custom Layout',
+ *             'img' => get_stylesheet_directory_uri() . '/img/layout.gif',
  *         ],
  *     ],
  *     GenesisLayout::UNREGISTER => [
- *         GenesisLayout::SIDEBAR_CONTENT,
  *         GenesisLayout::CONTENT_SIDEBAR_SIDEBAR,
  *         GenesisLayout::SIDEBAR_CONTENT_SIDEBAR,
  *         GenesisLayout::SIDEBAR_SIDEBAR_CONTENT,
  *     ],
+ *     GenesisLayout::DEFAULT_LAYOUT => __genesis_return_full_width_content(),
  * ];
  *
  * return [
@@ -42,11 +42,12 @@ namespace SeoThemes\Core;
  */
 class PageLayouts extends Component {
 
-	const REGISTER                = 'register';
-	const UNREGISTER              = 'unregister';
-	const FULL_WIDTH_CONTENT      = 'full-width-content';
-	const CONTENT_SIDEBAR         = 'content-sidebar';
-	const SIDEBAR_CONTENT         = 'sidebar-content';
+	const REGISTER = 'register';
+	const UNREGISTER = 'unregister';
+	const DEFAULT_LAYOUT = 'default-layout';
+	const FULL_WIDTH_CONTENT = 'full-width-content';
+	const CONTENT_SIDEBAR = 'content-sidebar';
+	const SIDEBAR_CONTENT = 'sidebar-content';
 	const CONTENT_SIDEBAR_SIDEBAR = 'content-sidebar-sidebar';
 	const SIDEBAR_CONTENT_SIDEBAR = 'sidebar-content-sidebar';
 	const SIDEBAR_SIDEBAR_CONTENT = 'sidebar-sidebar-content';
@@ -58,11 +59,59 @@ class PageLayouts extends Component {
 	 */
 	public function init() {
 		if ( array_key_exists( self::REGISTER, $this->config ) ) {
-			array_map( 'genesis_register_layout', array_keys( $this->config[ self::REGISTER ] ), $this->config[ self::REGISTER ] );
+			$this->register( $this->config[ self::REGISTER ] );
 		}
 
 		if ( array_key_exists( self::UNREGISTER, $this->config ) ) {
-			array_map( 'genesis_unregister_layout', $this->config[ self::UNREGISTER ] );
+			$this->unregister( $this->config[ self::UNREGISTER ] );
 		}
+
+		if ( array_key_exists( self::DEFAULT_LAYOUT, $this->config ) ) {
+			$this->set_default( $this->config[ self::DEFAULT_LAYOUT ] );
+		}
+	}
+
+	/**
+	 * Register Genesis page layouts.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $layouts Layouts to register.
+	 *
+	 * @return void
+	 */
+	protected function register( $layouts ) {
+		array_walk( $layouts, function ( $args ) {
+			\genesis_register_layout( $args['id'], $args );
+		} );
+	}
+
+	/**
+	 * Unregister Genesis page layouts.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $layouts Layouts to unregister.
+	 *
+	 * @return void
+	 */
+	protected function unregister( $layouts ) {
+		array_map( 'genesis_unregister_layout', $layouts );
+	}
+
+	/**
+	 * Set a default Genesis layout.
+	 *
+	 * Allow a user to identify a layout as being the default layout on a new
+	 * install, as well as serve as the fallback layout.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $layout Layout handle.
+	 *
+	 * @return void
+	 */
+	protected function set_default( $layout ) {
+		\genesis_set_default_layout( $layout );
 	}
 }
